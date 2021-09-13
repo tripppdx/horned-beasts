07b3c43afe8fc380c776ce16fdc28090b7560061
Tripp Lucas <tripppdx@gmail.com>
2021-09-13 11:56:01 -0700

    initial, but woefully late files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
+import Header from "./Header.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main message={"Hi there"} />
+      <Footer />
     </div>
   );
+}
+
+class Footer extends Component {
+  render() {
+    return <h1>I am a footer</h1>;
+  }
+}
+
+class Main extends Component {
+  // needs to know the title pass in somehow...
+  render() {
+    return (
+      // <div>
+      //   <h1>I am Main my message is {this.props.message}</h1>
+      //   <HornedBeast />
+      // </div>
+      <>
+        <h1>I am Main my message is {this.props.message}</h1>
+        <HornedBeast title={"Rhino"} />
+      </>
+    );
+  }
+}
+
+class HornedBeast extends Component {
+  render() {
+    return <h1>I am a horned beast with title of {this.props.title}</h1>;
+  }
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return <h1>I am a header</h1>;
+  }
+}
+
+export default Header;
